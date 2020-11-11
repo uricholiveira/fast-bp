9f3c235158c9948456a62d2fc0807ee5b7c9ac5a
Urich Oliveira <oliveira.urich@gmail.com>
2020-11-11 00:17:04 -0300

    Função para gerar modelos -> através do app.ext.db

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
 
-from app.routes import user as user_ns, workspace as workspace_ns
-from app.ext.db import SessionLocal, engine
-from app.models import user, workspace
+from app.routes import user as user_ns, workspace as workspace_ns, environment as environment_ns
+from app.ext.db import SessionLocal, engine, generate_models
+from app.models import user, workspace, environment
 
-workspace.Base.metadata.create_all(bind=engine)
-user.Base.metadata.create_all(bind=engine)
+generate_models()
 
 app = FastAPI()
 
 app.include_router(user_ns.router, prefix="/user", tags=["User"])
 app.include_router(workspace_ns.router, prefix="/workspace", tags=["Workspace"])
+app.include_router(environment_ns.router, prefix="/environment", tags=["Environment"])
 
 if __name__ == '__main__':
 	uvicorn.run(app, host="127.0.0.1", port=8000)
--- app/ext/db.py
+++ app/ext/db.py
@@ -1,7 +1,12 @@
+import logging
+import os
+from app import models
 from dynaconf import settings
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+logging.getLogger().setLevel('INFO')
 
 check_same_thread = False if str(settings.SQLALCHEMY_URL).startswith("sqlite") else True
 engine = create_engine(settings.SQLALCHEMY_URL, connect_args={"check_same_thread": check_same_thread})
@@ -18,3 +23,12 @@ def get_db():
 		yield db
 	finally:
 		db.close()
+
+
+# Generate all models
+def generate_models():
+	logging.info("Creating database models...")
+	models.user.Base.metadata.create_all(bind=engine)
+	models.workspace.Base.metadata.create_all(bind=engine)
+	models.environment.Base.metadata.create_all(bind=engine)
+	logging.info("Database models created!")
